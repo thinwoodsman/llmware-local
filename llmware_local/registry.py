35531d1ecc3eb14e65f6410ec29bb34e71872121
Thin Woodsman <oroboros@hotmail.com>
2024-07-03 23:50:18 -0400

    added basic utils for working with local model catalog

--- llmware_local/registry.py
+++ llmware_local/registry.py
@@ -13,6 +13,8 @@ from llmware.models import ModelCatalog
 from llmware.configs import LLMWareConfig
 
 _local_model_catalog = "local_llmware_model_catalog.json"
+_last_used_model_dir = LLMWareConfig.get_model_repo_path()
+_last_used_model_file = _local_model_catalog
 
 # Note: this will create the catalog if it does not exist
 def load_local_model_catalog(cat_dir=LLMWareConfig.get_model_repo_path(), cat_file=_local_model_catalog):
@@ -24,9 +26,12 @@ def load_local_model_catalog(cat_dir=LLMWareConfig.get_model_repo_path(), cat_fi
         # Create local catalog from built-in if it doesn't exist
         mc.save_model_registry(fp=cat_dir, fn=cat_file)
     mc.load_model_registry(fp=cat_dir, fn=cat_file)
+    # pretty unsafe if the same process opens multiple catalogs: be warned
+    _last_used_model_dir = cat_dir
+    _last_used_model_file = cat_file
     return mc
 
-def save_local_model_catalog(mc, cat_dir=LLMWareConfig.get_model_repo_path(), cat_file=_local_model_catalog, create_backup=True):
+def save_local_model_catalog(mc, cat_dir=_last_used_model_dir, cat_file=_last_used_model_file, create_backup=True):
     if not os.path.exists(cat_dir):
         os.makedirs(cat_dir)
 
@@ -36,3 +41,5 @@ def save_local_model_catalog(mc, cat_dir=LLMWareConfig.get_model_repo_path(), ca
 
     mc.save_model_registry(fp=cat_dir, fn=cat_file)
 
+def local_model_catalog():
+    return (_last_used_model_dir,_last_used_model_file)
